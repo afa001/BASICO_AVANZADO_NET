48f1999631ada9f90986f4beed798f9a11451b98
Anderson Alarcon <anderson.alarcon.afa@hotmail.com>
2020-09-17 14:53:42 -0500

    back - tema implementacion de interfaces

---
+++ basicoAvanzado(.NET FRAMEWORK)/AvisosVarios/AvisosTrafico.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvisosVarios
+{
+    class AvisosTrafico : IAvisos
+    {
+        private string remitente;
+        private string mensaje;
+        private string fecha;
+
+        public AvisosTrafico() {
+            remitente = "DGT";
+            mensaje = "Sancion cometida. Pague antes de 3 dias y se beneficiará de una reduccion en la sancion";
+            fecha = "";
+        } 
+
+        public AvisosTrafico(string _remitente, string _mensaje, string _fecha)
+        {
+            remitente = _remitente;
+            mensaje = _mensaje;
+            fecha = _fecha;
+        }
+
+        public string GetFecha()
+        {
+            return fecha;
+        }
+
+        public void MostrarAviso()
+        {
+            Console.WriteLine(mensaje);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ basicoAvanzado(.NET FRAMEWORK)/AvisosVarios/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvisosVarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AvisosTrafico avisosTrafico = new AvisosTrafico();
+
+            avisosTrafico.MostrarAviso();
+
+            AvisosTrafico avisosTrafico2 = new AvisosTrafico("Jefatura Provinicial","Sancion de velocidad: USD300","02-03-2020");
+            
+            Console.WriteLine(avisosTrafico2.GetFecha());
+            avisosTrafico2.MostrarAviso();
+        }
+    }
+}
